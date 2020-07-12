6f755b9d39e5ed5192d0206615f51640e87ecc81
DieVlogger <67368155+DieVlogger@users.noreply.github.com>
2020-07-12 14:13:50 +0200

    Add files via upload

---
+++ bannend words.js
@@ -0,0 +1,9 @@
+if (message.send) {
+    'Hurensohn;missgeburt;mösenknecht'
+} else {message.delete
+    
+} 
+    
+    
+message.channel.send; channel_id=717475417367248978 
+({user}, 'Diese Nachricht wurde gelöscht wegen BadWordUsage')
