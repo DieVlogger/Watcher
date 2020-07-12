810a69d96d150ca7ddfb76d3e431e499760dda21
DieVlogger <67368155+DieVlogger@users.noreply.github.com>
2020-07-12 16:23:18 +0200

    Delete ping.js

--- commands/ping.js
+++
@@ -1,9 +0,0 @@
-module.exports = {
-    name: 'ping',
-    description: 'Ping!',
-    cooldown: 5,
-    execute(message) {
-        // teamBOT BOTCOMMANDS TESTBOTCOMMANDS
-        message.channel.send('Pong.');
-    },
-};
