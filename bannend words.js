882020f296524a5ecb6e7687671b3e32a5568958
DieVlogger <67368155+DieVlogger@users.noreply.github.com>
2020-07-12 14:13:29 +0200

    Delete bannend words.js

--- bannend words.js
+++
@@ -1,9 +0,0 @@
-if (user.send) {
-    'Hurensohn;missgeburt;mösenknecht'
-} else {message.delete
-    
-} 
-    
-    
-message.channel.send; channel_id=717475417367248978 
-('Diese Nachricht wurde gelöscht wegen BadWordUsage')
